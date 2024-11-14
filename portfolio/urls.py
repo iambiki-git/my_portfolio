b48c3e5b730ce32d70cccb3ad8d9efe19c480237
Bikram Gurung <grgbiki.git@gmail.com>
2024-11-14 22:41:45 +0545

    add project and details

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,9 @@ from portfolio import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about-me/', views.about, name='about'),
+    path('my-projects/', views.my_project, name='project'),
+    path('project/fashion-mart-details/',views.fashion_mart_details, name='fashionmart-details'),
+    path('project/blog-details/',views.blog_details, name='blog-detail'),
+    path('project/note-details/',views.note_details, name='note-detail'),
+    path('contact-me/', views.contact, name='contact'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -6,3 +6,19 @@ def index(request):
 
 def about(request):
     return render(request, 'portfolio/about.html')
+
+def my_project(request):
+    return render(request, 'portfolio/project_list.html')
+
+def fashion_mart_details(request):
+    return render(request, 'portfolio/fashionmart_details.html')
+
+def blog_details(request):
+    return render(request, 'portfolio/blog_details.html')
+
+def note_details(request):
+    return render(request, 'portfolio/note_details.html')
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
+
